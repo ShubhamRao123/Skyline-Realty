886c284211d767dc8609747309277ea3e031865c
Shubham Rao <shubhamrao834@gmail.com>
2024-05-26 09:47:27 +0530

    Add about section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Skyline Realty</h1>
+      <p className="mb-4 mt-6 text-slate-800">
+        Skyline Realty is a premier real estate agency specializing in assisting
+        clients with buying, selling, and renting properties in highly
+        sought-after neighborhoods. Our team of seasoned agents is committed to
+        delivering exceptional service, ensuring a seamless experience
+        throughout the buying and selling process.
+      </p>
+      <p className="mb-4 text-slate-800">
+        Our mission is to assist our clients in achieving their real estate
+        goals through expert advice, personalized service, and an in-depth
+        knowledge of the local market. Whether you are buying, selling, or
+        renting a property, we are here to support you every step of the way.
+      </p>
+      <p className="mb-4 text-slate-800">
+        Our team of agents has a wealth of experience and knowledge in the real
+        estate industry, and we are committed to providing the highest level of
+        service to our clients. We believe that buying or selling a property
+        should be an exciting and rewarding journey, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p>
+    </div>
+  );
 }
